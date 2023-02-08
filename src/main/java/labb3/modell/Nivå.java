c8fcd0a66f900634dbf6244132415d2b430fe3d0
Olle Ronstad <olle.ronstad@gmail.com>
2023-02-08 16:59:48 +0100

    implemented nivå

--- src/main/java/labb3/modell/Nivå.java
+++ src/main/java/labb3/modell/Nivå.java
@@ -1,50 +1,59 @@
 package labb3.modell;
 
 import java.util.ArrayList;
-import labb3.vy.Målarduk;
+import java.util.Observable;
 
-// TODO: Gör så att klassen Nivå ärver Observable i paketet java.util. 
-public class Nivå {
-
-    private Rum startRum;
+public class Nivå extends Observable {
     private ArrayList<Rum> rum;
     private Rum nuvarandeRum;
-    // TODO: Lägg till tillståndsvariabler för att hålla reda på nivåns rum och
-    // i vilket rum som användaren "är".
 
     public Nivå(Rum startRum, ArrayList<Rum> rum) {
-        this.startRum = startRum;
+        this.nuvarandeRum = startRum;
         this.rum = rum;
         if (!rum.contains(startRum)) {
             throw new Error("Arraylis rum does not contain startrum");
         }
-        for (Rum r : rum) {
-            // TODO: Kontrollera att inga rum överlappar varandra. Om det ändå är
-            // fallet, kasta undantag med lämpligt felmeddelande.
+
+        // loops through all roms and check if any one overlaps and throws error if so
+        for (Rum i : rum) {
+            for (Rum j : rum) {
+                if (i != j) {
+                    int x_dist = i.getX() - j.getX();
+                    int y_dist = i.getY() - j.getY();
+                    if (Math.abs(x_dist) < i.getWith() / 2 + j.getWith() / 2) {
+                        throw new Error("rum överlappar");
+                    } else if (y_dist < i.getHeight() / 2 + j.getHeight() / 2) {
+                        throw new Error("rum överlappar");
+                    }
+                }
+
+            }
         }
 
     }
 
-    public ArrayList<Rum> getAllRums(Målarduk målarduk) {
-        // TODO: Skriv en instansmetod som returnerar alla rummen. Denna behöver
-        // Målarduk för att veta vilka rum som finns på nivån och som ska ritas ut.
-        return null;
+    public ArrayList<Rum> getAllRums() {
+        return this.rum;
     }
 
     public Rum getCurentRum() {
-        // TODO Skriv en instansmetod som returnerar en referens till det rum som
-        // användaren "är i".
-        return null;
+        return this.nuvarandeRum;
     }
 
     public void hoppaÅt(Väderstreck väderstreck) {
-        // TODO: Skriv klar instansmetoden hoppaÅt nedan så att den ändrar det rum
-        // som användaren "är i" om det är möjligt genom att följa en gång från
-        // rummet och i riktning väderstreck.
-
         // Om väderstreck inte är en riktning i vilken det finns en gång, så ändras
         // inte rummet användaren "är i" (och inte heller kastas undantag). (Denna
         // metod använder kontrolldelen av programmet för att begära ett hopp till
         // angränsande rum efter att användaren tryckt på en tangent.)
+        if (this.nuvarandeRum.finnsUtgångÅt(väderstreck)) {
+            Gång path = this.nuvarandeRum.gångÅt(väderstreck);
+            Rum nextRum;
+            if (path.getFrån() == this.nuvarandeRum) {
+                nextRum = path.getTill();
+            } else {
+                nextRum = path.getFrån();
+            }
+            this.nuvarandeRum = nextRum;
+        }
     }
 }
