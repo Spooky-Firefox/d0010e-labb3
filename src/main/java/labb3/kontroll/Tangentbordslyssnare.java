44d3034e615b3d44c6c8296ab49473754c5dc8b4
svenviktorm <viktormagnusson73@gmail.com>
2023-02-08 10:12:17 +0100

    Merge origin/master  gjorde tangentbordlysnare Conflicts: 	.idea/misc.xml 	pom.xml

--- src/main/java/labb3/kontroll/Tangentbordslyssnare.java
+++ src/main/java/labb3/kontroll/Tangentbordslyssnare.java
@@ -4,36 +4,39 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import labb3.modell.Nivå;
+import labb3.modell.Väderstreck;
 
 public class Tangentbordslyssnare implements KeyListener {
-	private Nivå enNivå;
 
-	public Tangentbordslyssnare(Nivå enNivå) {
-		this.enNivå = enNivå;
-	}
+    private Nivå enNivå;
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO: Skriv klar denna metod som automatiskt anropas så snart
-		// användaren tryckt på en tangent på tangentbordet. Metoden ska
-		// översätta tangenten till ett väderstreck och sen anropa hoppaÅt i
-		// enNivå med detta väderstreck. Här ska
-		//
-		// w betyda "hoppa åt NORR",
-		// d betyda "hoppa åt ÖSTER",
-		// s betyda "hoppa åt SÖDER" och
-		// a betyda "hoppa åt VÄSTER".
-	}
+    public Tangentbordslyssnare(Nivå enNivå) {
+        this.enNivå = enNivå;
+    }
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// Används inte men måste implementeras eftersom keyTyped finns i
-		// gränssnittet KeyListener.
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char c = e.getKeyChar();
+        switch (c) {
+            case 'w' -> enNivå.hoppaÅt(Väderstreck.NORR);
+            case 'a' -> enNivå.hoppaÅt(Väderstreck.VÄSTER);
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// Används inte men måste implementeras eftersom keyReleased finns is
-		// gränssnittet KeyListener.
-	}
+            case 's' -> enNivå.hoppaÅt(Väderstreck.SÖDER);
+            case 'd' -> enNivå.hoppaÅt(Väderstreck.ÖSTER);
+        }
+        System.out.println("keyPressed: " + c);
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Används inte men måste implementeras eftersom keyTyped finns i
+        // gränssnittet KeyListener.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Används inte men måste implementeras eftersom keyReleased finns is
+        // gränssnittet KeyListener.
+    }
 }
